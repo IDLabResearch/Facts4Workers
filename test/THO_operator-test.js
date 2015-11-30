3b38b0b2281c9014b53859641aae0ef99f0d9966
Joachim Van Herwegen <joachimvh@gmail.com>
2015-11-30 11:33:57 +0100

    JSONLDParser tests, initial THO operator test interface.

---
+++ test/THO_operator-test.js
@@ -0,0 +1,14 @@
+
+var assert = require('assert');
+var _ = require('lodash');
+var RESTdesc = require('../RESTdesc');
+
+function relative (relativePath)
+{
+    return path.join(__dirname, relativePath);
+}
+
+describe('RESTdesc', function ()
+{
+    var restdesc = new RESTdesc([relative('n3/thermolympics_operator_new/api.n3')], relative('n3/thermolympics_operator_new/goal.n3'));
+});
