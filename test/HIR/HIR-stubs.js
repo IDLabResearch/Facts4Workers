a65a814c0cd546d5721c8807c3c8aee95a0b985a
Joachim Van Herwegen <joachimvh@gmail.com>
2017-04-28 15:25:32 +0200

    Fixed URIs for HIR use case

--- test/HIR/HIR-stubs.js
+++ test/HIR/HIR-stubs.js
@@ -2,20 +2,20 @@ var assert = require('assert');
 
 var stubs = {
     GET : {
-        'https://offsug.hir.facts4.work/api/v1/parts': parts,
-        'https://offsug.hir.facts4.work/api/v1/parts/5b015ac5-fd14-488c-b387-2d8d7b5d4989': part,
-        'https://offsug.hir.facts4.work/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=1&value=6': offset,
-        'https://offsug.hir.facts4.work/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=2&value=8.2': offset,
-        'https://offsug.staging.facts4.work/api/v1/parts': parts,
-        'https://offsug.staging.facts4.work/api/v1/parts/5b015ac5-fd14-488c-b387-2d8d7b5d4989': part,
-        'https://offsug.staging.facts4.work/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=1&value=6': offset,
-        'https://offsug.staging.facts4.work/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=2&value=8.2': offset,
+        'https://staging.facts4.work/staging/offsug/api/v1/parts': parts,
+        'https://staging.facts4.work/staging/offsug/api/v1/parts/5b015ac5-fd14-488c-b387-2d8d7b5d4989': part,
+        'https://staging.facts4.work/staging/offsug/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=1&value=6': offset,
+        'https://staging.facts4.work/staging/offsug/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=2&value=8.2': offset,
+        'https://staging.facts4.work/staging/offsug/api/v1/parts': parts,
+        'https://staging.facts4.work/staging/offsug/api/v1/parts/5b015ac5-fd14-488c-b387-2d8d7b5d4989': part,
+        'https://staging.facts4.work/staging/offsug/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=1&value=6': offset,
+        'https://staging.facts4.work/staging/offsug/api/v1/offsets?part_id=5b015ac5-fd14-488c-b387-2d8d7b5d4989&dimension_id=2&value=8.2': offset,
     },
     POST: {
         'https://auth.facts4.work/oauth/token': oauth
     },
     PATCH: {
-        'https://offsug.staging.facts4.work/api/v1/parts/5b015ac5-fd14-488c-b387-2d8d7b5d4989': part,
+        'https://staging.facts4.work/staging/offsug/api/v1/parts/5b015ac5-fd14-488c-b387-2d8d7b5d4989': part,
     }
 };
 
